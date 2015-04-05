bf9fdbc003ef6ff4ae667afb34616fc0aa8025f6
peterjwest <peterjwest3@gmail.com>
2015-04-05 15:43:57 +0100

    Added code coverage (failing)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,25 @@
+var _ = require('lodash');
+var fs = require('fs');
 var gulp = require('gulp');
+var gulpIstanbul = require('gulp-istanbul');
+var istanbul = require('istanbul');
 var jscs = require('gulp-jscs');
+var mocha = require('gulp-mocha');
+var path = require('path');
+var reporter = require('istanbul-text-full-reporter');
 
 gulp.task('standards', function() {
-    return gulp.src(['lib/*.js', '*.js'])
+    return gulp.src(['lib/**/*.js', '*.js'])
         .pipe(jscs());
+});
+
+gulp.task('coverage', function(cb) {
+    return gulp.src(['lib/**/*.js', '*.js'])
+        .pipe(gulpIstanbul({ includeUntested: true }))
+        .pipe(gulpIstanbul.hookRequire())
+        .on('finish', function() {
+            gulp.src(['test/**/*.js', '!gulpfile.js'])
+                .pipe(mocha())
+                .pipe(gulpIstanbul.writeReports({ reporters: [reporter] }))
+        });
 });
